42c9622eb8f8a49da3c70d8af4b24826836010b5
Vincenzo Picone <vincenzopicone88@icloud.com>
2022-12-19 16:18:08 +0100

    CSS modificato

--- Assets/JS/file.js
+++ Assets/JS/file.js
@@ -1,29 +1,23 @@
-
 // window.addEventListener("load", (event) => {
 
 // })
 
 // button js
 
-const buttonSel = document.querySelector("#bottone1")
+const buttonSel = document.querySelector("#bottone1");
 
+buttonSel.setAttribute("disabled", "");
 
-buttonSel.setAttribute("disabled", "")
+buttonSel.addEventListener("click", (event) => {
+  console.log("pressed button");
+});
 
-buttonSel.addEventListener("click", event => {
-    console.log("pressed button")
-})
-
-document.querySelector("#input1").addEventListener("change", event => {
-
-})
-
+document.querySelector("#input1").addEventListener("change", (event) => {});
 
 // logic change disable to able button
 
-const checkedSel = document.querySelector("#input1").addEventListener("change", event => {
-    buttonSel.toggleAttribute("disabled")
-})
-
-
-
+const checkedSel = document
+  .querySelector("#input1")
+  .addEventListener("change", (event) => {
+    buttonSel.toggleAttribute("disabled");
+  });

9be5c26a23aceb1e3bcb7409c15888855c895161
Carlos Sarmiento <c.sarmientobaca@gmail.com>
2022-12-19 14:34:51 +0100

    added disabled button

--- Assets/JS/file.js
+++ Assets/JS/file.js
@@ -3,19 +3,32 @@
 
 // })
 
-window.addEventListener("click", () => {
-
-})
-
-document.querySelector("#bottone1").addEventListener("click", event => {
-    console.log("u clicked")
+const buttonSel = document.querySelector("#bottone1")
 
 
+buttonSel.setAttribute("disabled", "")
 
-
+buttonSel.addEventListener("click", event => {
+    console.log("pressed button")
 })
 
 document.querySelector("#input1").addEventListener("change", event => {
-    console.log("u checked ")
 
 })
+
+
+
+actionButton = () => {
+
+    const buttonSel = document.querySelector("#bottone1")
+    buttonSel.setAttribute("disabled", "")
+    const checkedSel = document.querySelector("#input1").addEventListener("change", event => {
+
+        buttonSel.classList.toggle("off")
+    })
+
+
+
+
+
+}

86b19d5deeec4f093c9e712fedfd9eec63711bfd
SamueleRusco <119084990+SamueleRusco@users.noreply.github.com>
2022-12-20 14:23:40 +0100

    partially done benchmark js

--- Assets/JS/benchmarkFile.js
+++ Assets/JS/benchmarkFile.js
@@ -2,8 +2,131 @@
 //   return alert("asdffafadssda");
 // });
 
-const buttonPress = document.querySelector("#benchmarkBottone");
+// const buttonPress = document.querySelector("#benchmarkBottone");
 
-buttonPress.addEventListener("click", (event) => {
-  buttonPress.classList.toggle("selected");
-});
+// buttonPress.addEventListener("click", (event) => {
+//   buttonPress.toggle("selected");
+// });
+// const padreDomande = document.getElementById("bottoni");
+const questions = [
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question: "What does CPU stand for?",
+    correct_answer: "Central Processing Unit",
+    incorrect_answers: [
+      "Central Process Unit",
+      "Computer Personal Unit",
+      "Central Processor Unit",
+    ],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
+    correct_answer: "Final",
+    incorrect_answers: ["Static", "Private", "Public"],
+  },
+  {
+    category: "Science: Computers",
+    type: "boolean",
+    difficulty: "easy",
+    question: "The logo for Snapchat is a Bell.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+  {
+    category: "Science: Computers",
+    type: "boolean",
+    difficulty: "easy",
+    question:
+      "Pointers were not used in the original C programming language; they were added later on in C++.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "What is the most preferred image format used for logos in the Wikimedia database?",
+    correct_answer: ".svg",
+    incorrect_answers: [".png", ".jpeg", ".gif"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question: "In web design, what does CSS stand for?",
+    correct_answer: "Cascading Style Sheet",
+    incorrect_answers: [
+      "Counter Strike: Source",
+      "Corrective Style Sheet",
+      "Computer Style Sheet",
+    ],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "What is the code name for the mobile operating system Android 7.0?",
+    correct_answer: "Nougat",
+    incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question: "On Twitter, what is the character limit for a Tweet?",
+    correct_answer: "140",
+    incorrect_answers: ["120", "160", "100"],
+  },
+  {
+    category: "Science: Computers",
+    type: "boolean",
+    difficulty: "easy",
+    question: "Linux was first created as an alternative to Windows XP.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "Which programming language shares its name with an island in Indonesia?",
+    correct_answer: "Java",
+    incorrect_answers: ["Python", "C", "Jakarta"],
+  },
+];
+
+/*---BOTTONI---*/
+risposte = () => {
+  for (i = 0; i < 3; i++) {
+    const newButton = document.createElement("button");
+    newButton.classList.add("bottoneSbagliato");
+    document.getElementById("bottoni").appendChild(newButton);
+    newButton.innerHTML = questions[0].incorrect_answers[i];
+  }
+  const newButton = document.createElement("button");
+  newButton.classList.add("bottoneGiusto");
+  newButton.innerHTML = questions[0].correct_answer;
+  document.getElementById("bottoni").appendChild(newButton);
+};
+/*-----TITOLO------*/
+titolo = () => {
+  const newTitle = document.createElement("h1");
+  newTitle.innerHTML = questions[0].question;
+  document.getElementById("titoloBenchmark").appendChild(newTitle);
+};
+/*-------QUESTION----*/
+
+window.onload = () => {
+  risposte();
+
+  titolo();
+};

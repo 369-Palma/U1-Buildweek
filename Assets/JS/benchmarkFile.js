ba4a242cde1b75fe47e1f3bb4e21b013a6c36b5b
SamueleRusco <119084990+SamueleRusco@users.noreply.github.com>
2022-12-20 10:53:25 +0100

    minor changes

---
+++ Assets/JS/benchmarkFile.js
@@ -0,0 +1,9 @@
+// window.addEventListener("load", (event) => {
+//   return alert("asdffafadssda");
+// });
+
+const buttonPress = document.querySelectorAll(".benchmarkBottone");
+
+function AA = buttonPress.addEventListener("click", (event) => {
+  console.log("bottone premuto");
+});

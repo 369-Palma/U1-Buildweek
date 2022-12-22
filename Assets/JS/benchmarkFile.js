55821175f2ab7a89d90bfdc85d590c4e517a81a4
Palma Iacobelli <palmaiacobelli92@gmail.com>
2022-12-22 16:42:54 +0100

    Ultima versione corretta results Page

--- Assets/JS/benchmarkFile.js
+++ Assets/JS/benchmarkFile.js
@@ -159,64 +159,24 @@ function shuffle(array) {
   return array;
 }
 
-//>>> ************* timer ****************>>>
-
-// // tempo attuale in numero con gettime + 60secs (1000 =1sec)
-// let end = new Date().getTime() + 60000;
-// // fa intervali di updates ogni secondo ... sotto }, 1000)
-// let repeat = setInterval(function () {
-//   // Get today's date and time
-//   let now = new Date().getTime();
-
-//   // quanto tempo manca tra end e now, tempo attuale e il tempo attuale piu 6
-//   let tempoManca = end - now;
-
-//   // matematica di cambio secondi minuti
-//   let minutes = Math.floor((tempoManca % (1000 * 60 * 60)) / (1000 * 60));
-//   let seconds = Math.floor((tempoManca % (1000 * 60)) / 1000);
-
-//   // Output
-//   document.getElementById("timer").innerHTML = "";
-//   document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
-
-//   // se il timer è 0 cambia il tiemr a next qeustion
-//   if (tempoManca < 0) {
-//     clearInterval(repeat);
-//     document.getElementById("timer").innerHTML = "next question";
-//     document.getElementById("timer").innerHTML = "";
-//   }
-// }, 1000);
-
-//<<< ************* timer ****************<<<
 const risposte = (posizione, domande) => {
   //>>> ************* timer ****************>>>
+  // const timer = () => {
+  //   // tempo attuale in numero con gettime + 60secs (1000 =1sec)
+  //   let end = new Date().getTime() + 60000;
+  //   // fa intervali di updates ogni secondo ... sotto }, 1000)
+  //   let repeat = setInterval(function () {
 
-  // tempo attuale in numero con gettime + 60secs (1000 =1sec)
-  let end = new Date().getTime() + 60000;
-  // fa intervali di updates ogni secondo ... sotto }, 1000)
-  let repeat = setInterval(function () {
-    // Get today's date and time
-    let now = new Date().getTime();
+  //     // Get today's date and time
+  //     let now = new Date().getTime();
 
-    // quanto tempo manca tra end e now, tempo attuale e il tempo attuale piu 6
-    let tempoManca = end - now;
+  //     // quanto tempo manca tra end e now, tempo attuale e il tempo attuale piu 6
+  //     let tempoManca = end - now;
 
-    // matematica di cambio secondi minuti
-    let minutes = Math.floor((tempoManca % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((tempoManca % (1000 * 60)) / 1000);
+  //     // matematica di cambio secondi minuti
+  //     let minutes = Math.floor((tempoManca % (1000 * 60 * 60)) / (1000 * 60));
+  //     let seconds = Math.floor((tempoManca % (1000 * 60)) / 1000);
 
-    // Output
-    document.getElementById("timer").innerHTML = "";
-    document.getElementById("timer").innerHTML =
-      minutes + "m " + seconds + "s ";
-
-    // se il timer è 0 cambia il tiemr a next qeustion
-    if (tempoManca < 0) {
-      clearInterval(repeat);
-      document.getElementById("timer").innerHTML = "next question";
-      document.getElementById("timer").innerHTML = "";
-    }
-  }, 1000);
   //     // Output
   //     document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
 
@@ -233,8 +193,7 @@ const risposte = (posizione, domande) => {
   //<<< ************* timer ****************<<<
 
   //inseriti parametri (obbligatori, in questo modo quando chiamo "counterIncrease", posso dirgli quale risposta caricare)
-  let possibleAnswers = [];
-  //array che conterrà i bottoni
+  let possibleAnswers = []; //array che conterrà i bottoni
   document.getElementById("bottoni").innerHTML = ""; //pulizia div "bottoni"
 
   for (i = 0; i < domande[posizione].incorrect_answers.length; i++) {
@@ -290,8 +249,20 @@ const risposte = (posizione, domande) => {
     // per ogni elemento lo aggiungo al div bottoni (dopo averli mischiati)
     document.getElementById("bottoni").appendChild(element);
   });
+  const newTimer = document.createElement("div");
+  newTimer.id = "timer";
+  newTimer.classList.add("tempo");
+  document.getElementById("contenitoreTimer");
+  setTimeout(() => {
+    if (posizione < domande.length - 1) {
+      risposteIncorrette.push(null);
+      counterIncrease();
+    } else {
+      risposteIncorrette.push(null);
+      lastQuestion();
+    }
+  }, 1000);
 };
-
 /*-------------------TOGGLE DEI TASTI---------------------------------*/
 const aggiungiClasseSelected = (event) => {
   const elementoCliccato = event.target;

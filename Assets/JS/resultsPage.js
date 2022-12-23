c6576138ec4ffa1f8ff4f6853618c43880e7a238
Vincenzo Picone <vincenzopicone88@icloud.com>
2022-12-23 13:50:25 +0100

    Fix Codice Benchmark

--- Assets/JS/resultsPage.js
+++ Assets/JS/resultsPage.js
@@ -1,4 +1,4 @@
-// ************** CODICE PER LA PAGINA CHE RICEVE I DATI  VARIABILI DA CAMBIARE***************
+// ************** CODICE PER LA PAGINA CHE RICEVE I DATI  letIABILI DA CAMBIARE***************
 // questo mi cerca nel url del browser
 const paginaDiPrima = window.location.search;
 
@@ -7,18 +7,13 @@ const url = new URLSearchParams(paginaDiPrima);
 const corrette = parseInt(url.get("correct"));
 const sbagliate = parseInt(url.get("wrong"));
 
-let textCenter = ""
+let textCenter = "";
 
 if (corrette > 5) {
-
-  textCenter = `Congratulations!`
+  textCenter = `Congratulations!`;
+} else {
+  textCenter = `Oh no! Try again`;
 }
-else {
-
-  textCenter = `Oh no! Try again`
-
-}
-
 
 // >>>>>>*********** Plugin **************>>>>>
 
@@ -26,33 +21,33 @@ Chart.pluginService.register({
   beforeDraw: function (chart) {
     if (chart.config.options.elements.center) {
       // Get ctx from string
-      var ctx = chart.chart.ctx;
+      let ctx = chart.chart.ctx;
 
       // Get options from the center object in options
-      var centerConfig = chart.config.options.elements.center;
-      var fontStyle = centerConfig.fontStyle || 'Arial';
-      var txt = centerConfig.text;
-      var color = centerConfig.color || '#000';
-      var maxFontSize = centerConfig.maxFontSize || 75;
-      var sidePadding = centerConfig.sidePadding || 20;
-      var sidePaddingCalculated = (sidePadding / 100) * (chart.innerRadius * 2)
+      let centerConfig = chart.config.options.elements.center;
+      let fontStyle = centerConfig.fontStyle || "Arial";
+      let txt = centerConfig.text;
+      let color = centerConfig.color || "#000";
+      let maxFontSize = centerConfig.maxFontSize || 75;
+      let sidePadding = centerConfig.sidePadding || 20;
+      let sidePaddingCalculated = (sidePadding / 100) * (chart.innerRadius * 2);
       // Start with a base font of 30px
       ctx.font = "30px " + fontStyle;
 
       // Get the width of the string and also the width of the element minus 10 to give it 5px side padding
-      var stringWidth = ctx.measureText(txt).width;
-      var elementWidth = (chart.innerRadius * 2) - sidePaddingCalculated;
+      let stringWidth = ctx.measureText(txt).width;
+      let elementWidth = chart.innerRadius * 2 - sidePaddingCalculated;
 
       // Find out how much the font can grow in width.
-      var widthRatio = elementWidth / stringWidth;
-      var newFontSize = Math.floor(30 * widthRatio);
-      var elementHeight = (chart.innerRadius * 2);
+      let widthRatio = elementWidth / stringWidth;
+      let newFontSize = Math.floor(30 * widthRatio);
+      let elementHeight = chart.innerRadius * 2;
 
       // Pick a new font size so it will not be larger than the height of label.
-      var fontSizeToUse = Math.min(newFontSize, elementHeight, maxFontSize);
-      var minFontSize = centerConfig.minFontSize;
-      var lineHeight = centerConfig.lineHeight || 25;
-      var wrapText = false;
+      let fontSizeToUse = Math.min(newFontSize, elementHeight, maxFontSize);
+      let minFontSize = centerConfig.minFontSize;
+      let lineHeight = centerConfig.lineHeight || 25;
+      let wrapText = false;
 
       if (minFontSize === undefined) {
         minFontSize = 20;
@@ -64,10 +59,10 @@ Chart.pluginService.register({
       }
 
       // Set font settings to draw it correctly.
-      ctx.textAlign = 'center';
-      ctx.textBaseline = 'middle';
-      var centerX = ((chart.chartArea.left + chart.chartArea.right) / 2);
-      var centerY = ((chart.chartArea.top + chart.chartArea.bottom) / 2);
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      let centerX = (chart.chartArea.left + chart.chartArea.right) / 2;
+      let centerY = (chart.chartArea.top + chart.chartArea.bottom) / 2;
       ctx.font = fontSizeToUse + "px " + fontStyle;
       ctx.fillStyle = color;
 
@@ -76,18 +71,18 @@ Chart.pluginService.register({
         return;
       }
 
-      var words = txt.split(' ');
-      var line = '';
-      var lines = [];
+      let words = txt.split(" ");
+      let line = "";
+      let lines = [];
 
       // Break words up into multiple lines if necessary
-      for (var n = 0; n < words.length; n++) {
-        var testLine = line + words[n] + ' ';
-        var metrics = ctx.measureText(testLine);
-        var testWidth = metrics.width;
+      for (let n = 0; n < words.length; n++) {
+        let testLine = line + words[n] + " ";
+        let metrics = ctx.measureText(testLine);
+        let testWidth = metrics.width;
         if (testWidth > elementWidth && n > 0) {
           lines.push(line);
-          line = words[n] + ' ';
+          line = words[n] + " ";
         } else {
           line = testLine;
         }
@@ -95,18 +90,16 @@ Chart.pluginService.register({
 
       centerY -= (lines.length / 2) * lineHeight;
 
-      for (var n = 0; n < lines.length; n++) {
+      for (let n = 0; n < lines.length; n++) {
         ctx.fillText(lines[n], centerX, centerY);
         centerY += lineHeight;
       }
       //Draw text in center
       ctx.fillText(line, centerX, centerY);
     }
-  }
+  },
 });
 // <<<<<<<*********** Plugin **************<<<<<
-
-
 
 //creo il grafico
 
@@ -117,12 +110,6 @@ Chart.defaults.global.defaultFontSize = 18;
 Chart.defaults.global.defaultFontColor = "white";
 
 let risposte = new Chart(myChart, {
-
-
-
-
-
-
   type: "doughnut",
   data: {
     labels: ["Wrong", "Correct"],
@@ -143,12 +130,12 @@ let risposte = new Chart(myChart, {
     elements: {
       center: {
         text: textCenter,
-        color: '#FF6384', // Default is #000000
-        fontStyle: 'Arial', // Default is Arial
+        color: "#FF6384", // Default is #000000
+        fontStyle: "Arial", // Default is Arial
         sidePadding: 15, // Default is 20 (as a percentage)
         minFontSize: 18, // Default is 20 (in px), set to false and text will not wrap.
         lineHeight: 20, // Default is 25 (in px), used for when text wraps
-      }
+      },
     },
     title: {
       display: true,
@@ -162,7 +149,6 @@ let risposte = new Chart(myChart, {
     },
   },
 });
-
 
 const totale = corrette + sbagliate;
 const percentualeCorrette = ((corrette / totale) * 100).toFixed(1);
@@ -181,13 +167,11 @@ document.getElementById("numeroSbagliate").innerHTML =
   sbagliate + "/" + totale + " questions";
 
 if (percentualeCorrette >= 60) {
-
   document.getElementById("resultsrisultatoTestoBlu").innerText =
     "You passed the exam.";
   document.getElementById("resultstestoCerchio").innerText =
     "We'll send you the certificate in few minutes.";
 } else {
-
   document.getElementById("resultsrisultatoTestoBlu").innerText =
     "You didn't pass the exam.";
   document.getElementById("resultstestoCerchio").innerText = "";

118ab73d092fef36b1e17e1b90af1018b29d34cf
SamueleRusco <119084990+SamueleRusco@users.noreply.github.com>
2022-12-23 17:44:36 +0100

    some fixes

--- Assets/JS/benchmarkFile.js
+++ Assets/JS/benchmarkFile.js
@@ -94,6 +94,11 @@ const questions = [
     incorrect_answers: ["Python", "C", "Jakarta"],
   },
 ];
+/*--------COSTANTI E VARIABILI-----------*/
+
+let risposteIncorrette = [];
+let risposteCorrette = [];
+
 /*-----TITOLO------*/
 titolo = (posizione, domande) => {
   document.getElementById("titoloBenchmark").innerHTML = "";
@@ -102,9 +107,6 @@ titolo = (posizione, domande) => {
   document.getElementById("titoloBenchmark").appendChild(newTitle);
 };
 /*----------ARRAY DI RISPOSTE CORRETTE E NON CORRETTE-----------*/
-
-let risposteIncorrette = [];
-let risposteCorrette = [];
 
 const incorrette = (question) => {
   risposteIncorrette.push(question);
@@ -115,7 +117,7 @@ const corrette = (question) => {
 };
 /*----------------CAST DEI BOTTONI-----------------------------*/
 const risposte = (posizione, domande) => {
-  timer(); //inseriti parametri (obbligatori, in questo modo quando chiamo "counterIncrease", posso dirgli quale risposta caricare)
+  //inseriti parametri (obbligatori, in questo modo quando chiamo "counterIncrease", posso dirgli quale risposta caricare)
   let possibleAnswers = []; //array che conterrà i bottoni
   document.getElementById("bottoni").innerHTML = ""; //pulizia div "bottoni"
 
@@ -200,12 +202,14 @@ function shuffle(array) {
   }
   return array;
 }
+
 //TOGGLE
 let counter = 0;
 const aggiungiClasseSelected = (event) => {
   const elementoCliccato = event.target;
   elementoCliccato.classList.toggle("selected");
 };
+
 //AUMENTO DEL COUNTER
 const counterIncrease = () => {
   //messo sopra counter++ cosi incrementa il valore prima di fare tutto
@@ -219,122 +223,123 @@ const counterIncrease = () => {
 const lastQuestion = () => {
   gotoResults();
 };
+
 /*-------TIMER---------*/
-function timer() {
-  const FULL_DASH_ARRAY = 283;
-  const WARNING_THRESHOLD = 10;
-  const ALERT_THRESHOLD = 5;
+// function timer() {
+//   const FULL_DASH_ARRAY = 283;
+//   const WARNING_THRESHOLD = 10;
+//   const ALERT_THRESHOLD = 5;
 
-  const COLOR_CODES = {
-    info: {
-      color: "green",
-    },
-    warning: {
-      color: "green",
-      threshold: WARNING_THRESHOLD,
-    },
-    alert: {
-      color: "green",
-      threshold: ALERT_THRESHOLD,
-    },
-  };
+//   const COLOR_CODES = {
+//     info: {
+//       color: "green",
+//     },
+//     warning: {
+//       color: "green",
+//       threshold: WARNING_THRESHOLD,
+//     },
+//     alert: {
+//       color: "green",
+//       threshold: ALERT_THRESHOLD,
+//     },
+//   };
 
-  const TIME_LIMIT = 20;
-  let timePassed = 0;
-  let timeLeft = TIME_LIMIT;
-  let timerInterval = null;
-  let remainingPathColor = COLOR_CODES.info.color;
+//   const TIME_LIMIT = 20;
+//   let timePassed = 0;
+//   let timeLeft = TIME_LIMIT;
+//   let timerInterval = null;
+//   let remainingPathColor = COLOR_CODES.info.color;
 
-  document.getElementById("app").innerHTML = `
-<div class="base-timer">
-  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-    <g class="base-timer__circle">
-      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
-      <path
-        id="base-timer-path-remaining"
-        stroke-dasharray="283"
-        class="base-timer__path-remaining ${remainingPathColor}"
-        d="
-          M 50, 50
-          m -45, 0
-          a 45,45 0 1,0 90,0
-          a 45,45 0 1,0 -90,0
-        "
-      ></path>
-    </g>
-  </svg>
-  <span id="base-timer-label" class="base-timer__label">${formatTime(
-    timeLeft
-  )}</span>
-</div>
-`;
+//   document.getElementById("app").innerHTML = `
+// <div class="base-timer">
+//   <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+//     <g class="base-timer__circle">
+//       <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+//       <path
+//         id="base-timer-path-remaining"
+//         stroke-dasharray="283"
+//         class="base-timer__path-remaining ${remainingPathColor}"
+//         d="
+//           M 50, 50
+//           m -45, 0
+//           a 45,45 0 1,0 90,0
+//           a 45,45 0 1,0 -90,0
+//         "
+//       ></path>
+//     </g>
+//   </svg>
+//   <span id="base-timer-label" class="base-timer__label">${formatTime(
+//     timeLeft
+//   )}</span>
+// </div>
+// `;
 
-  startTimer();
+//   startTimer();
 
-  function onTimesUp() {
-    clearInterval(timerInterval);
-  }
+//   function onTimesUp() {
+//     clearInterval(timerInterval);
+//   }
 
-  function startTimer() {
-    timerInterval = setInterval(() => {
-      timePassed = timePassed += 1;
-      timeLeft = TIME_LIMIT - timePassed;
-      document.getElementById("base-timer-label").innerHTML =
-        formatTime(timeLeft);
-      setCircleDasharray();
-      setRemainingPathColor(timeLeft);
+//   function startTimer() {
+//     timerInterval = setInterval(() => {
+//       timePassed = timePassed += 1;
+//       timeLeft = TIME_LIMIT - timePassed;
+//       document.getElementById("base-timer-label").innerHTML =
+//         formatTime(timeLeft);
+//       setCircleDasharray();
+//       setRemainingPathColor(timeLeft);
 
-      if (timeLeft === 0) {
-        onTimesUp();
-      }
-    }, 1000);
-  }
+//       if (timeLeft === 0) {
+//         onTimesUp();
+//       }
+//     }, 1000);
+//   }
 
-  function formatTime(time) {
-    const minutes = Math.floor(time / 60);
-    let seconds = time % 60;
+//   function formatTime(time) {
+//     const minutes = Math.floor(time / 60);
+//     let seconds = time % 60;
 
-    if (seconds < 10) {
-      seconds = `0${seconds}`;
-    }
+//     if (seconds < 10) {
+//       seconds = `0${seconds}`;
+//     }
 
-    return `${minutes}:${seconds}`;
-  }
-  //possibilità di rimuoverlo
-  function setRemainingPathColor(timeLeft) {
-    const { alert, warning, info } = COLOR_CODES;
-    if (timeLeft <= alert.threshold) {
-      document
-        .getElementById("base-timer-path-remaining")
-        .classList.remove(warning.color);
-      document
-        .getElementById("base-timer-path-remaining")
-        .classList.add(alert.color);
-    } else if (timeLeft <= warning.threshold) {
-      document
-        .getElementById("base-timer-path-remaining")
-        .classList.remove(info.color);
-      document
-        .getElementById("base-timer-path-remaining")
-        .classList.add(warning.color);
-    }
-  }
-  //possibilità di rimuoverlo
+//     return `${minutes}:${seconds}`;
+//   }
+//   //possibilità di rimuoverlo
+//   function setRemainingPathColor(timeLeft) {
+//     const { alert, warning, info } = COLOR_CODES;
+//     if (timeLeft <= alert.threshold) {
+//       document
+//         .getElementById("base-timer-path-remaining")
+//         .classList.remove(warning.color);
+//       document
+//         .getElementById("base-timer-path-remaining")
+//         .classList.add(alert.color);
+//     } else if (timeLeft <= warning.threshold) {
+//       document
+//         .getElementById("base-timer-path-remaining")
+//         .classList.remove(info.color);
+//       document
+//         .getElementById("base-timer-path-remaining")
+//         .classList.add(warning.color);
+//     }
+//   }
+//   //possibilità di rimuoverlo
 
-  function calculateTimeFraction() {
-    const rawTimeFraction = timeLeft / TIME_LIMIT;
-    return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
-  }
+//   function calculateTimeFraction() {
+//     const rawTimeFraction = timeLeft / TIME_LIMIT;
+//     return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+//   }
 
-  function setCircleDasharray() {
-    const circleDasharray = `${(
-      calculateTimeFraction() * FULL_DASH_ARRAY
-    ).toFixed(0)} 283`;
-    document
-      .getElementById("base-timer-path-remaining")
-      .setAttribute("stroke-dasharray", circleDasharray);
-  }
-}
+//   function setCircleDasharray() {
+//     const circleDasharray = `${(
+//       calculateTimeFraction() * FULL_DASH_ARRAY
+//     ).toFixed(0)} 283`;
+//     document
+//       .getElementById("base-timer-path-remaining")
+//       .setAttribute("stroke-dasharray", circleDasharray);
+//   }
+// }
 
 /*-------WINDOW ONLOAD----*/
 
